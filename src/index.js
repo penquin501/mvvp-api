9be878b061192bae2942bdc45b33e129214a34ef
penquin501 <penquin501@gmail.com>
2022-11-10 14:56:36 +0700

    test connect db

--- src/index.js
+++ src/index.js
@@ -1,10 +1,27 @@
 const express = require("express");
+const mysql = require("mysql2");
 const app = express();
 const port = 3000;
 
-app.all("/", (req, res) => {
+const connection = mysql.createConnection(process.env.DATABASE_URL);
+console.log("Connected to PlanetScale!");
+connection.end();
+/**
+ * prod url: https://mvvp-api.cyclic.app/
+ * local url: http://localhost:3000/
+ */
+
+app.get("/", (req, res) => {
   console.log("Just got a request!");
   res.send("Yo!");
+});
+
+app.get("/test-connectdb", (req, res) => {
+  if (connection) {
+    res.send("connected");
+  } else {
+    res.send("error connect!");
+  }
 });
 
 console.log("running on port:" + port);
